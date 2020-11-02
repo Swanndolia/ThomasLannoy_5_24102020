1db37e7b05c7df97fcbd38ba7efa13e1921a6fc0
Swanndolia <Swanndolia@gmail.com>
2020-11-02 15:58:37 +0100

    add clear local & session storage

--- js/confirm.js
+++ js/confirm.js
@@ -1,3 +1,5 @@
 document.getElementById("main").innerHTML += ` <h2> Merci pour votre commande d'un montant de: ${sessionStorage.getItem(
   "orderTotalPrice"
 )}€ </h2><p>Votre identifiant de commande est le n° ${sessionStorage.getItem("orderId")}</p>`;
+localStorage.clear();
+sessionStorage.clear();
