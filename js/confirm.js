34d4c699e5a5114802759122c0c51227debbb8a2
Swanndolia <Swanndolia@gmail.com>
2020-11-04 19:43:38 +0100

    added title and fixed responsivbe

--- js/confirm.js
+++ js/confirm.js
@@ -1,4 +1,4 @@
-document.getElementById("main").innerHTML += ` <h2> Merci pour votre commande d'un montant de: ${sessionStorage.getItem(
+document.getElementById("main").innerHTML += ` <h2>pour votre commande d'un montant de: ${sessionStorage.getItem(
   "orderTotalPrice"
 )}€ </h2><p>Votre identifiant de commande est le n° ${sessionStorage.getItem("orderId")}</p>`;
 localStorage.clear();
